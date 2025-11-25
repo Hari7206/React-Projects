ba759e4d2727ea47f11b3941ef4fa0c31f4c7ef0
Hari Thapa <harithapa4654@gmail.com>
2025-11-25 23:49:04 +0530

    Margdarshak

---
+++ Margdarshak/src/component/Main/Main.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import './Main.css'
+import { assets } from '../../assets/assets'
+
+
+function Main() {
+    return (
+        <div className='main'>
+            <div className="nav">
+                <p>Margdarshak</p>
+                <img src={assets.user} alt="" />
+            </div>
+            <div className='main-container'>
+                <div className="greet">
+                    <p><span>Hello , hajur</span></p>
+                    <p>How can I help you today</p>
+                </div>
+                <div className="cards">
+                    <div className="card">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et dolor, fugit expedita voluptatum ipsa veritatis, </p>
+                        <img src={assets.compass_icon} alt="" />
+                    </div>
+                    <div className="card">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et dolor, fugit expedita voluptatum ipsa veritatis, </p>
+                        <img src={assets.bulb_icon} alt="" />
+                    </div>
+                    <div className="card">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et dolor, fugit expedita voluptatum ipsa veritatis, </p>
+                        <img src={assets.message_icon} alt="" />
+                    </div>
+                    <div className="card">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et dolor, fugit expedita voluptatum ipsa veritatis, </p>
+                        <img src={assets.code_icon} alt="" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Main
